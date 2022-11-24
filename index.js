06481749bb83b374d818b39a5003b45ef0050484
anandkumarmca6 <anandkumarmca6@gmail.com>
2022-11-24 16:18:03 +0530

    added atlas

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = 3001;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 const flash = require('connect-flash');
