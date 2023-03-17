6d75867311ed9d32a199fda82b0a1181dc11e504
Anand Kumar <97781086+kranandmca@users.noreply.github.com>
2023-03-17 13:43:06 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -6,7 +6,7 @@ const env = require('./environment');
 // mongoose.connect('mongodb://localhost/placement');
 // For atlas
 mongoose.connect(
-  'mongodb+srv://authentication:<password>@cluster0.fipg6ho.mongodb.net/?retryWrites=true&w=majority'
+  'mongodb+srv://placementapp:<password>@cluster0.cjmdero.mongodb.net/?retryWrites=true&w=majority'
 );
 const db = mongoose.connection;
 
